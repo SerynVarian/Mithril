2fa67df9fc1d4e4b5ce279a8b0c62ea48f2ae42b
SerynVarian <serynvarian@outlook.com>
2023-09-25 17:32:44 +0200

    Update

--- Mithril/build.zig
+++ Mithril/build.zig
@@ -2,54 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const mode = b.standardOptimizeOption();
 
-    // Add Static Library
-    const lib = b.addStaticLibrary(.{
-        .name = "MithrilLib",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(lib);
+    const exe = b.addExecutable("Mithril", "src/core/Entrypoint.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
 
-    // Add Executable
-    const exe = b.addExecutable(.{
-        .name = "MithrilExe",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
-
-    // Add Run Command for Executable
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // Create a step for unit testing the library
-    const lib_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_lib_tests = b.addRunArtifact(lib_tests);
-
-    // Create a step for unit testing the executable
-    const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    // Add Build Steps
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&run_lib_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    exe.install();
 }
--- Sandbox/src/main.zig
+++
@@ -1,5 +0,0 @@
-const SandboxApp = @import("SandboxApp.zig").SandboxApp;
-
-pub fn main() void {
-    SandboxApp.init();
-}
